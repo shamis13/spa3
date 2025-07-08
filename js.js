490fd9f68274b9b2ee1678c371f6a8f4216413d4
shamis13 <shamissalim53@gmail.com>
2025-07-08 23:59:51 +0400

    Add files via upload

---
+++ js.js
@@ -0,0 +1,8 @@
+function bookNow() {
+  alert("Thank you for booking with Serenity Spa! We'll contact you shortly.");
+}
+
+function submitForm(event) {
+  event.preventDefault();
+  alert("Your message has been sent. We'll get back to you soon!");
+}
